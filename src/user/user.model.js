645d6e9c1092b808381a8b4715d24ace3bb85047
ayogsynergy <sanchezg.eng@gmail.com>
2022-10-16 21:55:20 -0400

    user schema complete

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import * as crypto from "crypto";
 
 const schema = new mongoose.Schema({
    username:  {
@@ -42,4 +43,35 @@ const schema = new mongoose.Schema({
 );
 
 // virtual fields
+schema.virtual("password")
+    .set(function(password) {
+        this._password = password;
+        this.salt = this.makeSalt();
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function(){
+        return this._password
+    })
 // methods --> authenticate, encryptPassword, makeSalt
+schema.methods = {
+    encryptPassword: function(password) {
+        if(!password){
+            return "";
+        }
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return "";
+        }
+    },
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    },
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    }
+}
+
+export default mongoose.model("User", schema);
