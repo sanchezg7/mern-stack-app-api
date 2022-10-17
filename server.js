6efd02a9041528c8eb986e2d477b41ebfd4a6944
ayogsynergy <sanchezg.eng@gmail.com>
2022-10-16 22:41:32 -0400

    morgan isn't logging and neither is console.log, need to finish the module

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 import app from "./app.js";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+app.use(morgan(':id :method :url :response-time'))
+app.use(function (req, res, next) {
+   console.log(req.url);
+   next();
+});
+app.use(bodyParser.json());
+app.use(cors());
 
 const port = process.env.PORT || process.env.APP_PORT || 8080;
 
