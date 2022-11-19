3b7b3df3c24078a5250d8123b1d43720ce875a47
Gerardo Sanchez <sanchezg.eng@gmail.com>
2022-11-19 11:53:03 -0500

    add validation to user view model for endpoint

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,8 +1,9 @@
 import express from "express";
-
 const controller = express();
+import { userValidator } from "./user.viewmodel.js";
+import { runValidation } from "../validate.js";
 
-controller.post("/register", (req, res) => {
+controller.post("/register", userValidator, runValidation, (req, res) => {
     res.json({
         data: "success on register endpoint"
 
