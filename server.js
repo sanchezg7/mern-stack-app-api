3e9dd143ef9c602c4aa7e7bc7cbf616afd9c5c46
Gerardo Sanchez <sanchezg.eng@gmail.com>
2022-11-19 19:39:06 -0500

    integration with mongo atlas

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import app from "./app.js";
 import env from "./src/env.js";
+import mongoose from "mongoose";
 
 const port = env.PORT || env.APP_PORT || 8080;
+
+mongoose
+    .connect(env.DATABASE, {})
+    .then(() => console.log("DB Connected"))
+    .catch(err => console.error("DB Error => ", err));
 
 app.listen(port, () => {
    console.log("Running on Port: " + port);
