270ccd8369a51dd052e41fb2478bc0c7de504306
Gerardo Sanchez <sanchezg.eng@gmail.com>
2022-11-20 15:02:00 -0500

    send registration email with token

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -4,6 +4,9 @@ import { userValidator } from "./user.viewmodel.js";
 import { runValidation } from "../validate.js";
 import { SES } from "@aws-sdk/client-ses";
 import env from "../env.js";
+import User from "./user.model.js";
+// import {expressjwt as jwt} from "express-jwt";
+import jwt from "jsonwebtoken";
 
 const ses = new SES({
     region: env.AWS_REGION
@@ -15,6 +18,19 @@ const sendEmail = (sendEmailCommandInput) => {
 
 controller.post("/register", userValidator, runValidation, (req, res) => {
     const { name, email, password } = req.body;
+    let token;
+    User.findOne({email}).exec((err, user) => {
+       if(user) {
+           return res.status(400).json({
+               error: "Email is taken"
+           });
+       }
+       // generate token with username email and password
+        token = jwt.sign({ name, email, password }, env.JWT_ACCOUNT_ACTIVATION, {
+            expiresIn: "10m" // minutes
+        });
+
+
     const params = {
         Source: env.EMAIL_FROM,
         Destination: {
@@ -24,7 +40,12 @@ controller.post("/register", userValidator, runValidation, (req, res) => {
             Body: {
                 Html: {
                     Charset: "UTF-8",
-                    Data: `<html><body><h1>Hello ${name}</h1><p style="color:red">Test email.</p></body></html>`
+                    Data: `
+<html>
+    <h1>Verify your email address</h1>
+    <p>Please use the following linnk to complete your registration</p>
+    <p>${env.CLIENT_URL}/auth/activate/${token}</p>
+</html>`
                 }
             },
             Subject: {
@@ -46,6 +67,7 @@ controller.post("/register", userValidator, runValidation, (req, res) => {
             res.send("Email send failed");
         });
 
+    });
 
 });
 
