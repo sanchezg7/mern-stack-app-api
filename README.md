e0d227325de42de7fe00f7ec4dd9fe1bc35d265e
ayogsynergy <sanchezg.eng@gmail.com>
2022-12-28 17:19:51 -0500

    self gen the url and file key

--- README.md
+++ README.md
@@ -15,3 +15,55 @@ Delete all documents from a collection
 db.users.deleteMany({})
 
 # AWS Support
+Create respective role
+Create respective group
+Add group to role
+Add users to group
+
+Create a s3 bucket with the default settings
+Can also add a bucket policy for the admin user to put objects into the respective bucket
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AddCannedAcl",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::############:user/admin"
+            },
+            "Action": [
+                "s3:PutObject",
+                "s3:PutObjectAcl"
+            ],
+            "Resource": "arn:aws:s3:::bucket-name-here/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "public-read"
+                }
+            }
+        }
+    ]
+```
+Add respective CORS configuration in JSON format
+```json
+[
+  {
+    "AllowedHeaders": [
+      "*"
+    ],
+    "AllowedMethods": [
+      "PUT",
+      "POST",
+      "GET"
+    ],
+    "AllowedOrigins": [
+      "*"
+    ],
+    "ExposeHeaders": [],
+    "MaxAgeSeconds": 3000
+  }
+]
+```
+
+No need for inline policy, since the user was added to the group. The group assigned to the role.
--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -5,32 +5,81 @@ import { runValidation } from "../validate.js";
 import {adminMiddleware, enrichContextWithUser, requireSignIn} from "../user/authentication.middleware.js";
 import CategoryModel from "./category.model.js";
 import slugify from "slugify";
+import formidable from "formidable";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import env from "../env.js";
+import { v4 as uuidv4 } from 'uuid';
 
-controller.post("/", categoryCreateValidator, runValidation, requireSignIn, enrichContextWithUser, (req, res) => {
-    const { name, image, content } = req.body;
-    const slug = slugify(name);
+const s3 = new S3Client({
+    region: env.AWS_REGION
+})
+
+controller.post("/", /*categoryCreateValidator, runValidation,*/ requireSignIn, enrichContextWithUser, async (req, res) => {
+    // const { name, image, content } = req.body;
     const theImage = {
         url: "https://via.placeholder.com/850x150.png?text=domain.com",
         key: "123"
     };
 
-    const category = new CategoryModel({
-        name,
-        slug,
-        image: theImage,
-        content
-    });
-    category.postedBy = req.user._id;
-
-    category.save((err, data) => {
+    let form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
        if(err) {
-           const msg = "Category create failed";
-           console.log(msg, err);
-           res.status(500).json({
-               message: msg
-           })
+           console.log(err);
+           return res.status(400).json({
+               message: "Image could not upload"
+           });
        }
-       res.json(data);
+       const { image } = files;
+       const { name, content } = fields;
+       const slug = slugify(name);
+
+        if(image.size > 2000000) {
+            return res.status(400).json({
+                message: "Image is too big in size. Less than 2 MB"
+            });
+        }
+
+        const CATEGORY_DIRECTORY = "category";
+        const key = `${uuidv4()}.jpg`;
+        const params = {
+            "Bucket": "mern-stack-g-123",
+            "Key": `${CATEGORY_DIRECTORY}/${key}`,
+            "Body": image.path,
+            "ContentType": "image/jpg",
+        };
+
+        const BUCKET_NAME_REFACTOR_ME_TO_ENV = "mern-stack-g-123";
+        const putObjCmd = new PutObjectCommand(params);
+        s3.send(putObjCmd).then((data) => {
+            // there is limited metadata provided by the output of the command
+            console.log("AWS UPLOAD RES DATA", data);
+
+            const category = new CategoryModel({
+                name,
+                slug,
+                content
+            });
+            category.image.url = `https://${BUCKET_NAME_REFACTOR_ME_TO_ENV}.s3.amazonaws.com/${CATEGORY_DIRECTORY}/${key}`;
+            category.image.key = key;
+            category.postedBy = req.user._id;
+
+            category.save((err, data) => {
+               if(err) {
+                   const msg = "Category create failed";
+                   console.log(msg, err);
+                   res.status(500).json({
+                       message: msg
+                   })
+               }
+               res.json(data);
+            });
+        }).catch(err => {
+            if(err) {
+                return res.status(500).json({
+                    "message": "Image upload failed. Try again later."
+                });
+            }
+        });
     });
 });
 
