b5f2f60812c56345d4faec2797c663b8f6a577b6
ayogsynergy <sanchezg.eng@gmail.com>
2023-01-01 15:54:24 -0500

    changes to allow base64 image and basic content to be created for a category

--- src/category/category.vm.validator.js
+++ src/category/category.vm.validator.js
@@ -6,10 +6,11 @@ const categoryCreateValidator = [
         .isEmpty()
         .withMessage("Name is required."),
     check("image")
+        .not()
         .isEmpty()
         .withMessage("Image is required."),
     check("content")
-        .isLength({ min: 20 })
+        .isLength({ min: 5 })
         .withMessage("Content is required")
 ];
 
